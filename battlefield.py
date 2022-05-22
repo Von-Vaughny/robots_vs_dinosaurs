05f3c2d9c7894200c193a89c548d5836aca2025b
unknown <samuel.washington@snhu.edu>
2022-05-22 13:23:16 -0600

    Updated battlefield.py to show negative health as 0 hp remaining, to display winner, and to end battle phase before second player attacks if first player wins.

--- battlefield.py
+++ battlefield.py
@@ -15,22 +15,27 @@ class Battlefield:
         self.display_winner()
 
     def display_welcome(self):
-        print("Robot vs Dinosaur\n\nWelcome to the battle of the ages! There can only be one! Which side will win?")
+        print("Robot vs Dinosaur\n\nWelcome to the battle of the ages! There can only be one! Which side will win?\n")
 
+    # Add function to determine which player/group of players should go first.
     # def coin_toss(self):
     #     random.choice(['heads', 'tails'])
 
     # Add if statement to exit immediately when hp < 0 and to show hp as 0 remaining.
     def battle_phase(self):
-        while self.robot.health > 0 or self.dinosaur.health > 0:
+        while self.robot.health > 0 and self.dinosaur.health > 0:
             self.robot.attack(self.dinosaur)
-            print(f"\nRobot {self.robot.name} attacks {self.dinosaur.name} with {self.robot.active_weapon.name} for "\
+            print(f"Robot {self.robot.name} attacks {self.dinosaur.name} with {self.robot.active_weapon.name} for "\
                   f"{self.robot.active_weapon.attack_power} damage!")
-            print(f"Dinosaur {self.dinosaur.name} has {self.dinosaur.health} health remaining!\n")
+            print(f"Dinosaur {self.dinosaur.name} has {self.dinosaur.health if self.dinosaur.health > 0 else 0} health remaining!\n")
+            if self.dinosaur.health <= 0:
+                break
             self.dinosaur.attack(self.robot)
-            print(f"\nDinosaur {self.dinosaur.name} attacks {self.robot.name} for {self.dinosaur.attack_power} damage!")
-            print(f"Robot {self.robot.name} has {self.robot.health} health remaining!\n")
-
+            print(f"Dinosaur {self.dinosaur.name} attacks {self.robot.name} for {self.dinosaur.attack_power} damage!")
+            print(f"Robot {self.robot.name} has {self.robot.health if self.robot.health > 0 else 0} health remaining!\n")
 
     def display_winner(self):
-        pass
+        if self.robot.health <= 0:
+            print("Dinosaurs win!")
+        elif self.dinosaur.health <= 0: 
+            print("Robots win!")
