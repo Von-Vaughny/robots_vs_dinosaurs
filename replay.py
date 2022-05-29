5a2d79351bd8b58931f15dc610651b36d7caf367
unknown <samuel.washington@snhu.edu>
2022-05-29 01:20:51 -0600

    Corrected logic

--- replay.py
+++ replay.py
@@ -6,6 +6,6 @@ class Replay:
         self.cor_user_input = ""
 
     def replay(self):
-        while not len(self.cor_user_input):
+        while len(self.cor_user_input):
             self.cor_user_input = re.sub(r"[^0-1]", "", input("\nPlay again? (Input 0 for no, 1 for yes) "))
         return True if len(self.cor_user_input) else False
